4275f9bec35d7ad8e8b5f755614aed4fd266e63d
Carla <ncargar010@g.educaand.es>
2022-04-17 18:12:05 +0200

    preparado todo para la prueba

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 fun main() {
     println("Hello World!")
+    println(suma(1,2))
 }
 
 //Función de suma, hecha por Carla:
-
+fun suma(num1:Int,num2:Int):Int {
+    return num1+num2
+}
 //Función de multiplicación, hecha por Laura:
